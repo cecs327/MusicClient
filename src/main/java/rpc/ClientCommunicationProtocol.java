a44bb37ad9758e7799e50495133ee2bc79a6c47e
kylemazza <kyleamazza@gmail.com>
2019-03-11 14:00:57 -0700

    Adding vcs.xml

--- src/main/java/rpc/ClientCommunicationProtocol.java
+++ src/main/java/rpc/ClientCommunicationProtocol.java
@@ -5,27 +5,29 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class clientCommunicationProtocol {
+class clientCommunicationProtocol {
     static final int FRAGMENT_SIZE = 15000;                      // Packet size
+    private static final int TIMEOUT_DURATION = 10000;
     byte[] packetSize = new byte[FRAGMENT_SIZE];
 
-    private DatagramSocket mySocket = null;
-    private InetAddress ip = null;
+    private DatagramSocket socket;
+    private InetAddress ip;
     private int portNumber;
 
-    public void connect(int portNum) {
+    void connect(int portNum) {
         try {
             this.ip = InetAddress.getByName("localhost");    // Get localhost IP address
-            this.mySocket = new DatagramSocket();            // Initialize Socket
+            this.socket = new DatagramSocket();            // Initialize Socket
+            socket.setSoTimeout(TIMEOUT_DURATION);
             this.portNumber = portNum;                       // Initialize port number
-            //mySocket.connect(ip,portNum);//Not sure if this is needed
+            //socket.connect(ip,portNum);//Not sure if this is needed
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public String sendRequest(String request) {
+    String sendRequest(String request, String callSemantic) {
         String response = "";
         try {
 
@@ -33,13 +35,22 @@ public class clientCommunicationProtocol {
             requestPayload = request.getBytes();                                                                        // Fill payload
             DatagramPacket requestPacket = new DatagramPacket(requestPayload, requestPayload.length, this.ip, this.portNumber);     // Initialize request packet
             System.out.println("Client sending request packet.");
-            mySocket.send(requestPacket);                                                                               // Send request packet
+
+            switch(callSemantic) {
+                case "MAYBE":
+                    socket.send(requestPacket);
+                    break;
+                case "AT_LEAST_ONCE":
+                    
+            }
+
+            socket.send(requestPacket);                                                                               // Send request packet
             System.out.print("Client request packet sent.");
 
             byte[] responseData = new byte[FRAGMENT_SIZE];                                                              // Prepare response packet
             DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length);                      // Initialize reponse packet
             System.out.println("Client attempting to receive response packet.");
-            mySocket.receive(responsePacket);                                                                           // Retrieve reponse packet
+            socket.receive(responsePacket);                                                                           // Retrieve reponse packet
             System.out.println("Client has received response packet from server.");
             response = new String(responsePacket.getData());                                                            // Get reponse packet's payload
 
